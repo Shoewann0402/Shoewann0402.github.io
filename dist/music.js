2642c6037ad65543b079fc34a1a53de771eb9474
Shoewann <xuwang0402@gmail.com>
2019-07-21 17:13:49 +0800

    Site updated: 2019-07-21 17:13:38

---
+++ dist/music.js
@@ -0,0 +1,69 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "贝加尔湖畔",
+        artist: '纯音乐',
+        url: 'http://www.ytmp3.cn/down/57249.mp3',
+        cover: 'http://p2.music.126.net/vSdrZFQn3uMetLL_j3AnQg==/109951163432562414.jpg?param=130y130',
+      },			
+      {
+        name: "莫斯科郊外的晚上",
+        artist: '纯音乐',
+        url: 'http://www.ytmp3.cn/down/55877.mp3',
+        cover: 'http://p1.music.126.net/oMd7uEfKSUbyxWo2h2jNvA==/6068204674028750.jpg?param=130y130',
+      },		
+      {
+        name: "Faded",
+        artist: 'Alan Walker',
+        url: 'http://www.ytmp3.cn/down/57123.mp3',
+        cover: 'http://p2.music.126.net/8dzD62VK8jLDbhEqkmpIAg==/18277181788626198.jpg?param=130y130',
+      },			
+      {
+        name: "Sakura Tears",
+        artist: 'Nigel Silin',
+        url: 'http://www.ytmp3.cn/down/51498.mp3',
+        cover: 'http://p2.music.126.net/p-7pKS_S86OJo8cHcDNKFw==/109951163922821064.jpg?param=130y130',
+      },
+      {
+        name: "Friendships",
+        artist: 'Pascal Letoublon',
+        url: 'http://www.ytmp3.cn/down/46249.mp3',
+        cover: 'http://p2.music.126.net/ejBqv__hHneHWQnMd1x00A==/18338754440252475.jpg?param=130y130',
+      },
+      {
+        name: "The truth that you leave",
+        artist: 'Pianoboy',
+        url: 'http://www.ytmp3.cn/down/58893.mp3',
+        cover: 'http://p2.music.126.net/9idkdzbel_-lYBP7Dv_dVQ==/102254581395289.jpg?param=130y130',
+      },
+      {
+        name: "Journey",
+        artist: 'Capo Productions',
+        url: 'http://www.ytmp3.cn/down/51593.mp3',
+        cover: 'http://p1.music.126.net/x-LVbj4tX6oEDH3qkqJP2g==/663005511556827.jpg?param=130y130',
+      },
+      {
+        name: "River Flows In You",
+        artist: 'Yiruma',
+        url: 'http://www.ytmp3.cn/down/43385.mp3',
+        cover: 'http://p2.music.126.net/8ZRSyI0ZN_4ah8uzsNd1mA==/2324367581169008.jpg?param=130y130',
+      },
+      {
+        name: "Horizon",
+        artist: 'Janji',
+        url: 'http://www.ytmp3.cn/down/44211.mp3',
+        cover: 'http://p2.music.126.net/8pko-MebyO_1XlXms0rF9g==/109951163456840298.jpg?param=130y130',
+      },
+      {
+        name: "Intro",
+        artist: 'The xx',
+        url: 'http://www.ytmp3.cn/down/55076.mp3',
+        cover: 'http://p1.music.126.net/Xik6a3wXoGPOLI1GNXGdFQ==/825733232504415.jpg?param=130y130',
+      }
+    ]
+});
+
+
